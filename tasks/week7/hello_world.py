5b2cf98ebbacbf74e61e63a9cfa4851d4787a048
kaiwenzhang <kzhang1@uwyo.edu>
2025-03-07 13:23:57 -0700

    week 7

---
+++ tasks/week7/hello_world.py
@@ -0,0 +1,8 @@
+# This is a sample Python script.
+
+from HWs.hw1.check_airmass_project.airmasschecker.utils import calculate_min_airmass
+from tasks.week1.myprogram import myprogram
+
+
+if __name__ == '__main__':
+    myprogram()
